13a0afd2a478d0cde6e008fbdb4602e8e6877511
Shiv Matta <shiv.matta@gmail.com>
2022-04-26 16:39:31 -0700

    commit tree b472e32e9c1dcdf7dd1484a4d34751ae2ca697cf

---
+++ index.js
@@ -0,0 +1,30 @@
+function shout(string){
+    return string.toUpperCase();
+}
+
+function whisper(string){
+    return string.toLowerCase();
+}
+
+function logShout(string){
+    const spy = 'hello';
+    console.log(spy.toUpperCase());
+}
+
+function logWhisper(string){
+    const spy = 'HELLO';
+    console.log(spy.toLowerCase());
+}
+
+function sayHiToHeadphonedRoommate(string){
+    if (string === string.toLowerCase()){
+        return "I can't hear you!";
+    }
+        else if (string === string.toUpperCase()){
+            return "YES INDEED!";
+        }
+        else if (string === "Let's have dinner together!"){
+            return "I would love to!";
+        }
+    }
+
